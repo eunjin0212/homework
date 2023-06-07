06a2f2ed7fc1e5cf21ba114ebeccc83831651bac
eunjin0212 <eunjin9639@gmail.com>
2023-06-07 23:54:15 +0900

    🎉 Create Component file
    Input component, card component 파일 생성

---
+++ src/components/Card.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>card</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CardComponent',
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Input.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>input</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'InputComponent',
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
