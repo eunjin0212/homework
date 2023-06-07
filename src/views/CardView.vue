949326ea86f98e91762d9950a39643d0efa55f6d
eunjin0212 <eunjin9639@gmail.com>
2023-06-07 23:53:43 +0900

    🎉 Create View file
    Input view, card view 파일 생성

---
+++ src/views/CardView.vue
@@ -0,0 +1,17 @@
+<template>
+  <Card />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Card from '@/components/Card.vue';
+
+export default defineComponent({
+  name: 'CardView',
+  components: { Card },
+  setup() {
+    return {};
+  },
+});
+</script>
+<style lang="scss" scoped></style>
---
+++ src/views/InputView.vue
@@ -0,0 +1,18 @@
+<template>
+  <Input />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Input from '@/components/Input.vue';
+
+export default defineComponent({
+  name: 'CardView',
+  components: { Input },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
